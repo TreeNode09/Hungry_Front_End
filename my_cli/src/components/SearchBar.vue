2de935def8a87415ef54b285f43c9b17fdda3fb7
TreeNode09 <StickpageX@hotmail.com>
2024-06-27 21:14:02 +0800

    搜索界面添加搜索栏

--- my_cli/src/components/SearchBar.vue
+++ my_cli/src/components/SearchBar.vue
@@ -6,13 +6,16 @@
 import { ref } from 'vue'
 import router from '@/router'
 
+const emit = defineEmits(['update'])
 const searchText = ref('')
+
 function searchInfo(){
     if(searchText.value !== ''){
         router.push({
             path: '/search',
             query: {text: searchText.value}
-    })
+        })
+        emit('update')
     }
 }
 </script>
